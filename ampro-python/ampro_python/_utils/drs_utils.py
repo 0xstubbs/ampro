81c6a7f8361c0792a109096bd68922616a3504b6
Stubbs <stubbs0x@protonmail.com>
2024-02-03 03:13:40 -0700

    Working on drs_utils for using  to query the API

--- ampro-python/ampro_python/_utils/drs_utils.py
+++ ampro-python/ampro_python/_utils/drs_utils.py
@@ -1,8 +1,8 @@
 import os
+import json
 import requests
 import logging
 from dotenv import load_dotenv
-import logging
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -40,6 +40,7 @@ class DRSUtils:
             logger.info("There are no more docs to collect. Exiting...")
             # self.has_more_items = False
             self.paginate = False
+
     def _get_endpoint(self) -> str:
         if self.doc_type.lower() == "ad" or self.doc_type.lower() == "adfrawd":
             return "ADFRAWD"
@@ -51,32 +52,70 @@ class DRSUtils:
             logger.error(f"Invalid document type: {self.doc_type}")
             raise ValueError(f"Invalid document type: {self.doc_type}")
 
-            
+    def _get_summary_from_response(self, response) -> dict:
+        """
+        This function takes a request response and returns the summary.
+        """
+        logger.info("Processing response to get summary")
+
+        if not response.ok:
+            logger.error(f"Response return with status code {response.status_code}")
+            return {"Error": f"{response.status_code}"}
+        try:
+            summary = response.json().get("summary")
+            if summary is None:
+                raise ValueError("Response does not contain 'summary' key")
+            return {"summary": summary}
+        except json.JSONDecodeError:
+            logger.error(f"JSON decoding failed for {response}")
+            summary = {"Error": "Invalid JSON response"}
+        except ValueError as e:
+            logger.error(f"{e} in response: {response}")
+            summary = {"Error": str(e)}
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Request error: {e}")
+            summary = {"Error": "Request error"}
+
+        return summary
+
+    def _get_documents_from_response(self, response):
+        """
+        This function takes a Response and extracts the documents into list.
+        """
+        logging.info("Processing Response to get documents.")
+        if response.ok:
+            documents = response.json().get("documents")
+            self.list_of_docs.extend(documents)
+        else:
+            logging.error(f"Response Status Code: {response.status_code}")
+
     def _call_drs(self):
         logger.info(f"\n{'*'*50}\n_call_drs")
-        
+
         endpoint = self._get_endpoint()
-        headers = {"x-api-key": self.drs_api_key}
+        headers = {
+            "x-api-key": self.drs_api_key,
+            "USER_AGENT": "curl/7.68.0",
+        }
         payload = {
-            "offset": self.offset
-            # docLastModifiedDate = strptime(doc_last_modified, date_format)
+            "offset": self.offset,
+            "docLastModifiedDate": self.doc_last_modified,
         }
         request_url = f"{self.base_url}{endpoint}"
 
-        try:
-            logger.info("Calling API...")
-            r = requests.get(request_url, headers=headers, params=payload)
-        except Exception as e:
-            logger.exception(f"Exception: {e} while calling API")
-
-        return r
+        logger.info("Calling API...")
+        response = requests.get(request_url, headers=headers, params=payload)
+        logger.info(
+            f"Response URL: {response.url}\nResponse Headers: {response.headers}\nResponse Status: {response.status_code}"
+        )
+        return response
 
     def get_docs(
         self,
         doc_type: str = "AD",
         offset: int = 0,
-        doc_last_modified: str = None,
         paginate: bool = True,
+        doc_last_modified: str = None,
     ) -> dict:
         """
         This function collects all documents of a specified type.
@@ -91,37 +130,38 @@ class DRSUtils:
         EAD          ADFREAD        Emergency ADs
 
         """
-        logger.info(f"****\nget_docs\n\nDocument: {doc_type}\nOffset: {offset}\nPaginate: {paginate}")
-        self.offset = offset
-        self.doc_type = doc_type
-        self.paginate=paginate
-
         logger.info(
-            f"Calling `get_docs` with the following params: \ndoc_type: {self.doc_type}\noffset: {self.offset}"
+            f"****\nget_docs\n\nDocument: {doc_type}\nOffset: {offset}\n\nLast Modified Date Filter: {doc_last_modified}\nPaginate: {paginate}"
         )
 
+        self.offset = offset
+        self.doc_type = doc_type
+        self.paginate = paginate
+        self.doc_last_modified = doc_last_modified
+
+        # logger.info(
+        #     f"Calling `get_docs` with the following params: \ndoc_type: {self.doc_type}\noffset: {self.offset}"
+        # )
+
         while True:
-            self.paginate=paginate
+            self.paginate = paginate
             logger.info(f"\n\nPaginate: {self.paginate}\n\n")
-    
-            r = self._call_drs()
-    
-            if not r.raise_for_status():
-                r = r.json()
-                # logger.info(f"keys: {list(r.keys())}")
-                summary = r["summary"]
-                results = r["documents"]
-    
-                for doc in results:
-                    self.list_of_docs.append(doc)
-    
-                if self.paginate:
-                    self.paginate_docs(summary)
-                else:
-                    break
 
-        return self.list_of_docs
+            response = self._call_drs()
 
+            if response.ok:
+                summary = self._get_summary_from_response(response)
+                self._get_documents_from_response(response)
+
+                if not summary.get("hasMoreItems"):
+                    self.paginate = False
+                    logger.info(
+                        "There are no more pages left. Setting pagination = False"
+                    )
+            else:
+                logging.error(f"Response Status Code: {response.status_code}")
+                self.paginate = False
+                logging.info(f"Setting paginate to False...\nPaginate: {self.paginate}")
 
 
 # This part only runs when drs_utils.py is executed directly
